a329f5e4acc2d921aed83be1fe315abdc6febe2f
Christopher R. Hertel <crh@ubiqx.org>
2016-10-13 12:54:57 -0500

    SessionService: Converted 'const' declarations to enum.

--- nbt/SessionService.d
+++ nbt/SessionService.d
@@ -30,6 +30,7 @@
  * License:   GNU Lesser General Public License version 3
  * Standards: IETF RFC1001 and RFC1002 (STD#19)
  * Brief:     Support for the NBT Session Service
+ *
  * Details:
  *  The NBT Session Service provides for the establishment and maintenance
  *  of NetBIOS sessions over TCP, including the framing of NetBIOS session
@@ -42,10 +43,11 @@
  *
  *  The header is given in Network Byte Order, and is split into
  *  three subfields:
- *    - Type (8 bits)
- *    - Flags (7 bits)
- *    - Length (17 bits)
- *
+ *  <ul>
+ *    <li>Type (8 bits)
+ *    <li>Flags (7 bits)
+ *    <li>Length (17 bits)
+ *  </ul>
  *  In RFC1002, the Flags field is listed as being 8 bits wide, but the
  *  lowest order bit is a "length extension" which is used as the high-
  *  order bit of the Length field.  None of the other Flags bits have
@@ -55,22 +57,30 @@
  */
 module SessionService;
 
+// Enumerated Constants
+
 // Protocol Details
-const SS_PORT = 139;  /// The default NBT Session Service TCP listener port.
+enum ushort SS_PORT = 139;  /// Default Session Service TCP listener port.
 
 // Message Types
-const SS_SESSION_MESSAGE    = 0x00; /// Payload follows.
-const SS_SESSION_REQUEST    = 0x81; /// Request creation of a NetBIOS session.
-const SS_POSITIVE_RESPONSE  = 0x82; /// NetBIOS session accepted.
-const SS_NEGATIVE_RESPONSE  = 0x83; /// NetBIOS session denied.
-const SS_RETARGET_RESPONSE  = 0x84; /// NetBIOS session redirected.
-const SS_SESSION_KEEPALIVE  = 0x85; /// NBT session keep-alive.
+enum:ubyte
+  {
+  SS_SESSION_MESSAGE   = 0x00,  /// Payload follows.
+  SS_SESSION_REQUEST   = 0x81,  /// Request creation of a NetBIOS session.
+  SS_POSITIVE_RESPONSE = 0x82,  /// NetBIOS session accepted.
+  SS_NEGATIVE_RESPONSE = 0x83,  /// NetBIOS session denied.
+  SS_RETARGET_RESPONSE = 0x84,  /// NetBIOS session redirected.
+  SS_SESSION_KEEPALIVE = 0x85   /// NBT session keep-alive.
+  };
 
-// Error Codes
-const SS_ERR_NOT_LISTENING  = 0x80; /// Not Listening _On Called_ Name
-const SS_ERR_NOT_ANSWERING  = 0x81; /// Not Listening _For Calling_ Name
-const SS_ERR_NOT_PRESENT    = 0x82; /// Called Name Not Present
-const SS_ERR_INSUFFICIENT   = 0x83; /// Insufficient Resources
-const SS_ERR_UNSPECIFIED    = 0x8F; /// Unspecified Error
+// Negative Respoinse Error Codes
+enum:ubyte
+  {
+  SS_ERR_NOT_LISTENING = 0x80,  /// Not Listening _On Called_ Name
+  SS_ERR_NOT_ANSWERING = 0x81,  /// Not Listening _For Calling_ Name
+  SS_ERR_NOT_PRESENT   = 0x82,  /// Called Name Not Present
+  SS_ERR_INSUFFICIENT  = 0x83,  /// Insufficient Resources
+  SS_ERR_UNSPECIFIED   = 0x8F   /// Unspecified Error
+  };
 
 /* ================================= la fin ================================= */
