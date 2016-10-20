6393fb3d94ad00060148cfab454cfa3404f4b77a
Christopher R. Hertel <crh@ubiqx.org>
2016-10-20 15:56:30 -0500

    Test Toys: Added a directory for test code.
    Since this project is, among other things, a learning opportunity, I am placing working scratch code into this directory in hopes that it will be useful to others.

---
+++ testtoys/BigEndianExtraction.d
@@ -0,0 +1,82 @@
+/* Speed-test methods for extracting the NBT Session Service length field.
+ *
+ *  This is a test program.  It implements a small set of different
+ *  mechanisms for extacting a 17-bit LENGTH value from a big-endian
+ *  byte order 32-bit input.  The operations are repeated multiple
+ *  times on order to generate performance comparisons.
+ */
+
+enum elementCount = 100_000;
+enum testCount    =  10_000;
+
+uint extractLength( ubyte[4] a )
+  {
+  return( ((a[1] & 0x01) << 16) + (a[2] * 0x0100) + a[3] );
+  } /* extractLength */
+
+uint extractLength_bigEndianToNative( ubyte[4] a )
+  {
+  import std.bitmanip : bigEndianToNative;
+
+  return( bigEndianToNative!uint(a) & 0x1ffff );
+  } /* extractLength_bigEndianToNative */
+
+uint extractLength_bswap( ubyte[4] a ) // @nogc pure nothrow
+  {
+  version( LittleEndian )
+    {
+    import core.bitop : bswap;
+
+    return( bswap( *(cast(uint*)(a.ptr)) ) & 0x0001FFFF );
+    }
+  else
+    {
+    return( *(cast(uint*)(a.ptr)) & 0x0001FFFF );
+    }
+  } /* extractLength_bswap */
+
+unittest
+  {
+  import std.stdio : writeln;
+
+  ubyte[4] a = [ 0, 1, 2, 3 ];
+  const expected = 0x00010203;
+
+  writeln( "Unit Tests..." );
+  assert( extractLength( a ) == expected );
+  assert( extractLength_bigEndianToNative( a ) == expected );
+  assert( extractLength_bswap( a ) == expected );
+  writeln( "...done." );
+  } /* unittest */
+
+void main()
+  {
+  import std.stdio     : writefln;
+  import std.datetime  : benchmark, msecs;
+  import std.algorithm : map;
+
+  ubyte[4][] testArrays;
+  testArrays.length = elementCount;
+  ulong overallResult = 0;
+
+  auto test( alias func )()
+    {
+    ulong result = 0; // Ensure that the optimizer doesn't throw the code away.
+
+    foreach( element; testArrays )
+      {
+      result += func( element );
+      }
+    return( result );
+    } /* test */
+
+  const measurements
+    = benchmark!( () => overallResult += test!extractLength,
+                  () => overallResult += test!extractLength_bigEndianToNative,
+                  () => overallResult += test!extractLength_bswap )(testCount);
+
+  writefln("extractLength()                  : %s msecs",measurements[0].msecs);
+  writefln("extractLength_bigEndianToNative(): %s msecs",measurements[1].msecs);
+  writefln("extractLength_bswap()            : %s msecs",measurements[2].msecs);
+  writefln("overallResult: %s", overallResult);
+  } /* main */
---
+++ testtoys/CompOptimized.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+ProgName="BigEndianExtraction"
+#
+gdc ${ProgName}.d -o ${ProgName}
+echo "No Optimization at all."
+./${ProgName}
+#
+gdc -O1 ${ProgName}.d -o ${ProgName}
+echo "-O1 Optimization"
+./${ProgName}
+#
+gdc -O1 -fno-bounds-check -finline-functions ${ProgName}.d -o ${ProgName}
+echo "-O1 Optimization +(no bounds check, inline)"
+./${ProgName}
+#
+gdc -Os -fno-bounds-check -finline-functions ${ProgName}.d -o ${ProgName}
+echo "-Os Optimization +(no bounds check, inline)"
+./${ProgName}
+#
+gdc -O2 ${ProgName}.d -o ${ProgName}
+echo "-O2 Optimization"
+./${ProgName}
+#
+gdc -O2 -fno-bounds-check -finline-functions ${ProgName}.d -o ${ProgName}
+echo "-O2 Optimization +(no bounds check, inline)"
+./${ProgName}
+#
+gdc -O3 -fno-bounds-check -finline-functions ${ProgName}.d -o ${ProgName}
+echo "-O3 Optimization +(no bounds check, inline)"
+./${ProgName}
+
+
+# Strip ${ProgName}
+echo "Results of strip(1):"
+echo -n "Before: "
+ls -lh ${ProgName}
+strip ${ProgName}
+echo -n "After:  "
+ls -lh ${ProgName}
